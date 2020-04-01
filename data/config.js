e248b6d7b0973a4ae3b061e2b64bf0e80078e0dd
CodingNoye <codingnoye@kookmin.ac.kr>
2020-04-01 13:50:45 +0900

    머지: 날아간 data/config 재생성

---
+++ data/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    botname: '노예봇',
+    token: 'TOKEN HERE',
+    youtubeKey: 'TOKEN HERE', // Optional, 음악 패키지를 사용할 때만
+    defaultPrefix: '!',
+    defaultEnabled: ['기본', '음악']
+}
